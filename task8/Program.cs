5d7c8eae8db4a725e1a7bd593c6feba1e23323a2
Gennady <genchik111@yandex.ru>
2023-03-25 15:20:51 +0300

    solution task8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите любое число: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = 1;
+
+while (number1 >= number2)
+{
+   if (number2 % 2 == 0)
+   {
+      Console.Write("" + number2 + " ");
+   }
+   number2++;
+}
