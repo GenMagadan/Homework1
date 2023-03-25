d981c4873aff28a84596497eeba2a94441190740
Gennady <genchik111@yandex.ru>
2023-03-25 14:53:41 +0300

    solution task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+   Console.Write("Max = " + number1 + "");
+}
+else if (number2 > number1)
+{
+   Console.Write("Max = " + number2 + "");
+}
+else
+{
+   Console.Write("Числа равны");
+}
