5ec93a3dcd666c76ffd4edcbad76301901cae6c4
Gennady <genchik111@yandex.ru>
2023-03-25 15:11:30 +0300

    solution task6

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+   Console.Write("Число " + number + " - чётное ");
+}
+else
+{
+   Console.Write("Число " + number + " - нечётное ");
+}
