f4d0b1e79310097efa715a1f08543237cdc3db50
Gennady <genchik111@yandex.ru>
2023-05-12 18:18:57 +0300

    Add changes in task08

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Введите любое число: ");
+﻿// Напишите программу, которая на вход принимает число (N), а 
+// на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите любое число: ");
 int number1 = int.Parse(Console.ReadLine());
 int number2 = 1;
 
@@ -6,7 +11,7 @@ while (number1 >= number2)
 {
    if (number2 % 2 == 0)
    {
-      Console.Write("" + number2 + " ");
+      Console.Write($"{number2}, ");
    }
    number2++;
 }
