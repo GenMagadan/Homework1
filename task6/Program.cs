3749cea9f796ac0782647e4595cb345d99e0e03f
Gennady <genchik111@yandex.ru>
2023-05-12 18:14:35 +0300

    Add changes in task06

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,11 +1,17 @@
-﻿Console.Write("Введите любое число: ");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите любое число: ");
 int number = int.Parse(Console.ReadLine());
 
 if (number % 2 == 0)
 {
-   Console.Write("Число " + number + " - чётное ");
+   Console.Write($"Число {number} - чётное");
 }
 else
 {
-   Console.Write("Число " + number + " - нечётное ");
+   Console.Write($"Число {number} - нечётное");
 }
